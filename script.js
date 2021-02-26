207488bb346b9ada5033cc6bc7e866910c844d14
Ronda Heimberger <rheimber@gmail.com>
2021-02-26 11:23:48 -0500

    added local storage to js

--- script.js
+++ script.js
@@ -44,4 +44,13 @@ $(".time-block").each(function() {
         $(this).removeClass("past");
         $(this).addClass("future");
     }
+$("#hour9 .description") .val(localStorage.getItem("hour9"));
+$("#hour10 .description") .val(localStorage.getItem("hour10"));
+$("#hour11 .description") .val(localStorage.getItem("hour11"));
+$("#hour12 .description") .val(localStorage.getItem("hour12"));
+$("#hour13 .description") .val(localStorage.getItem("hour13"));
+$("#hour14 .description") .val(localStorage.getItem("hour14"));
+$("#hour15 .description") .val(localStorage.getItem("hour15"));
+$("#hour16 .description") .val(localStorage.getItem("hour16"));
+$("#hour17 .description") .val(localStorage.getItem("hour17"));
 })
